8f6114c9d79d43359d125e8a7b71a19b27dcb4e2
Ivan Egorov <fearntn@yandex.ru>
2020-07-10 12:44:54 +0300

    setted navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Navbar />
+    <router-view />
   </div>
 </template>
-
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  components: { Navbar }
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-
-#nav {
-  padding: 30px;
+body {
+  font-family: var(--main-cursive-font);
+  line-height: 1.4;
+  letter-spacing: 0.1rem;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+a {
+  text-decoration: none;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">
+      <div class="cart">
+        Cart
+        <span class="counter">{{counter}}</span>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "Navbar",
+  computed: mapGetters(["allCart"]),
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    ...mapActions(["getCart"])
+  },
+  created() {
+    this.getCart();
+    this.counter = this.allCart.length;
+  }
+};
+</script>
+
+<style scoped>
+#nav {
+  padding: 30px;
+  background-color: lightblue;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-height: 80px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.cart {
+  position: relative;
+  padding: 1rem;
+}
+.counter {
+  background-color: bisque;
+  padding: 0.05rem;
+  border-radius: 0.5rem;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+}
+</style>
