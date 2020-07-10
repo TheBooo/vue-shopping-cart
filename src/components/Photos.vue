b32e9befbf060320a705fd343b785639bf635572
Ivan Egorov <fearntn@yandex.ru>
2020-07-10 13:09:24 +0300

    display all photos

---
+++ src/components/Photos.vue
@@ -0,0 +1,72 @@
+<template>
+  <main class="photos-container">
+    <div v-for="photo in allPhotos" :key="photo.id" class="photo-container">
+      <div class="image-container">
+        <img :src="photo.url" :alt="photo.title" class="image" />
+      </div>
+      <div class="title">{{photo.title.slice(0, 20)}}</div>
+      <button>Add to cart</button>
+    </div>
+  </main>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "Photos",
+  computed: mapGetters(["allPhotos"]),
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapActions(["fetchPhotos"])
+  },
+  created() {
+    this.fetchPhotos();
+  }
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.5rem;
+  background-color: lightblue;
+  border: none;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #42b983;
+}
+
+.photos-container {
+  width: 90vw;
+  margin: 1rem auto;
+  max-width: 1170px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+  grid-column-gap: 1.5rem;
+  grid-row-gap: 2rem;
+}
+.photo-container {
+  border: 1px solid lightblue;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+.image-container {
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+}
+.image {
+  height: 14rem;
+  width: auto;
+}
+.title {
+  padding: 0.5rem;
+  font-weight: 500;
+  font-size: 1.2rem;
+  margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <Photos />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Photos from "../components/Photos";
 
 export default {
   name: "Home",
-  components: {}
+  components: { Photos }
 };
 </script>
